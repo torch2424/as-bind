f766cb6af265f24a334984ea4368b066c41a82a9
Aaron Turner <aaron@aaronthedev.com>
2019-08-28 00:35:15 -0700

    Fixed up the testing workflow

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+const assert = require('assert');
+const asbuild = require('../dist/asbuild.cjs');
+
+describe('asbind', () => {
+  it('should handle strings', () => {
+    console.log(asbuild);
+    assert.equal(true, true);
   });
 });
