675005176c8859b9e7ac4bfc49fcf09e779cde02
Aaron Turner <aaron@aaronthedev.com>
2019-09-02 15:11:53 -0700

    Made the regex

--- examples/markdown-parser/assembly/regex.ts
+++ examples/markdown-parser/assembly/regex.ts
@@ -33,40 +33,76 @@ function matchStar(pattern: string, text: string): i32 {
 }
 
 function matchGroup(pattern: string, text: string) {
-  const groupEnd = pattern.indexOf(")");
-  const groupPattern = pattern.slice(1, groupEnd);
-  if (pattern.charAt(groupEnd + 1) === "?") {
-    const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')?'
+  let groupEnd = pattern.indexOf(")");
+  let groupPattern = pattern.slice(1, groupEnd);
 
-    const doesGroupPatternMatch = match(
+  if (pattern.charAt(groupEnd + 1) === "?") {
+    let remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')?'
+
+    let doesGroupPatternMatch = match(
       groupPattern,
       text.slice(0, groupPattern.length)
     )
       ? 1
-      : 0;
-    const doesRemainderPatternWithoutGroupMatch = match(
+      : -1;
+    let doesRemainderPatternWithoutGroupMatch = match(
       remainderPattern,
       text.slice(groupPattern.length)
     )
       ? 1
-      : 0;
-    const doesRemainderPatternMatch = match(remainderPattern, text) ? 1 : 0;
+      : -1;
+    let doesRemainderPatternMatch = match(remainderPattern, text) ? 1 : -1;
+
+    if (
+      doesGroupPatternMatch > 0 &&
+      doesRemainderPatternWithoutGroupMatch > 0
+    ) {
+      return 1;
+    } else if (doesRemainderPatternMatch > 0) {
+      return 1;
+    }
+
+    return -1;
   } else if (pattern[groupEnd + 1] === "*") {
-    const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')*'
+    let remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')*'
 
-    return (
-      (match(groupPattern, text.slice(0, groupPattern.length)) &&
-        match(pattern, text.slice(groupPattern.length))) ||
-      match(remainderPattern, text)
-    );
-  } else {
-    const remainderPattern = pattern.slice(groupEnd + 1); // +1 needed to slice off the ')'
+    let doesGroupPatternMatch = match(
+      groupPattern,
+      text.slice(0, groupPattern.length)
+    )
+      ? 1
+      : -1;
+    let doesPatternWithoutGroupMatch = match(
+      pattern,
+      text.slice(groupPattern.length)
+    )
+      ? 1
+      : -1;
+    let doesRemainderPatternMatch = match(remainderPattern, text) ? 1 : -1;
 
-    return (
-      match(groupPattern, text.slice(0, groupPattern.length)) &&
-      match(remainderPattern, text.slice(groupPattern.length))
-    );
+    if (doesGroupPatternMatch > 0 && doesPatternWithoutGroupMatch > 0) {
+      return 1;
+    } else if (doesRemainderPatternMatch > 0) {
+      return 1;
+    }
+
+    return -1;
   }
+
+  let remainderPattern = pattern.slice(groupEnd + 1); // +1 needed to slice off the ')'
+
+  let doesGroupPatternMatch = match(
+    groupPattern,
+    text.slice(0, groupPattern.length)
+  )
+    ? 1
+    : -1;
+  let doesRemainderPatternMatch = match(remainderPattern, text) ? 1 : -1;
+
+  if (doesGroupPatternMatch && doesRemainderPatternMatch) {
+    return 1;
+  }
+  return -1;
 }
 
 export function match(pattern: string, text: string): i32 {
@@ -77,18 +113,18 @@ export function match(pattern: string, text: string): i32 {
     return matchStar(pattern, text);
   } else if (pattern.charAt(0) === "(") {
     return matchGroup(pattern, text);
-  } else {
-    return (
-      matchOne(pattern.charAt(0), text.charAt(0)) &&
-      match(pattern.slice(1), text.slice(1))
-    );
   }
+
+  return (
+    matchOne(pattern.charAt(0), text.charAt(0)) &&
+    match(pattern.slice(1), text.slice(1))
+  );
 }
 
 export function search(pattern: string, text: string): i32 {
   if (pattern.charAt(0) === "^") {
     return match(pattern.slice(1), text);
-  } else {
-    return match(".*" + pattern, text);
   }
+
+  return match(".*" + pattern, text);
 }
