d480b02e555aab7b4fb34f8581f3664831cc81ec
Aaron Turner <aaron@aaronthedev.com>
2019-09-16 00:29:50 -0700

    I am falling asleep hard but I did some of the reqired todo stuff

--- examples/markdown-parser/assembly/tokenizer.ts
+++ examples/markdown-parser/assembly/tokenizer.ts
@@ -22,174 +22,191 @@ function checkForTriplet(
   );
 }
 
-export function markdownTokenizer(markdown: string): void {
+export function markdownTokenizer(markdown: string): Array<string> {
   // TODO: I don't think array of objects work in AS?
   // Switch over to array of strings.
   // first string is index, second string is type, third string is value.
   // Then repeat until we get all of our tokens in an array
-  let tokens = new Array<Token>(0);
+  let tokenTuples = new Array<string>(0);
 
   for (let i: i32 = 0; i < markdown.length; i++) {
-    let token = new Token();
-    // Lastly, simply add the character
-    token.type = "Character";
-    token.index = i;
-    token.value = markdown.charAt(i);
-
-    // Pushing / Upadating the character token happens last
+    let tokenValue: string = markdown.charAt(i);
 
     // We care about newlines, as they specify blocks, and whether something is in the same newline
-    if (token.value.includes("\n")) {
-      token.type === "NewLine";
-      tokens.push(token);
+    if (tokenValue.includes("\n")) {
+      tokenTuples.push(i.toString());
+      tokenTuples.push("NewLine");
+      tokenTuples.push(tokenValue);
       continue;
     }
 
     // Check for whitespace
-    if (isWhitespace(token.value)) {
+    if (isWhitespace(currentCharacter)) {
       let tokenContinueLength = 0;
+      tokenValue = "";
       while (
         tokenContinueLength < markdown.length - i &&
         isWhitespace(markdown.charAt(i + tokenContinueLength))
       ) {
-        token.value += markdown.charAt(i + tokenContinueLength);
+        tokenValue += markdown.charAt(i + tokenContinueLength);
         tokenContinueLength += 1;
       }
-      i += tokenContinueLength;
 
-      token.type = "Whitespace";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Whitespace");
+      tokenTuples.push(tokenValue);
+      i += tokenContinueLength;
       continue;
     }
 
     // Check for the # Headers in the beginning of a line
-    if (token.value.includes("#")) {
-      token.type = "Header";
-      tokens.push(token);
+    if (tokenValue.includes("#")) {
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Header");
+      tokenTuples.push(tokenValue);
       continue;
     }
 
     // Check for Italics
-    if (token.value.includes("*") && markdown.charAt(i + 1).includes("*")) {
-      token.value = "**";
-      token.type = "Italics";
-      tokens.push(token);
+    if (tokenValue.includes("*") && markdown.charAt(i + 1).includes("*")) {
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Italics");
+      tokenTuples.push("**");
+
       i += 1;
       continue;
     }
 
     // Check for bold
-    if (token.value.includes("_") && markdown.charAt(i + 1).includes("_")) {
-      token.value = "__";
-      token.type = "Bold";
-      tokens.push(token);
+    if (tokenValue.includes("_") && markdown.charAt(i + 1).includes("_")) {
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Bold");
+      tokenTuples.push("__");
+
       i += 1;
       continue;
     }
 
     // Check for strikethrough
-    if (token.value.includes("~") && markdown.charAt(i + 1).includes("~")) {
-      token.value = "~~";
-      token.type = "Strikethrough";
-      tokens.push(token);
+    if (tokenValue.includes("~") && markdown.charAt(i + 1).includes("~")) {
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Strikethrough");
+      tokenTuples.push("~~");
+
       i += 1;
       continue;
     }
 
     // Check for Unordered List
-    if (token.value.includes("*") && isWhitespace(markdown.charAt(i + 1))) {
-      token.value = "* ";
-      token.type = "Unordered List";
-      tokens.push(token);
+    if (tokenValue.includes("*") && isWhitespace(markdown.charAt(i + 1))) {
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Unordered List");
+      tokenTuples.push("* ");
+
       i += 1;
       continue;
     }
 
     // Check for ordered list
     if (
-      token.value.includes("1") &&
+      tokenValue.includes("1") &&
       markdown.charAt(i + 1).includes(".") &&
       isWhitespace(markdown.charAt(i + 2))
     ) {
-      token.value = "1. ";
-      token.type = "Ordered List";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Ordered List");
+      tokenTuples.push("1. ");
+
       i += 1;
       continue;
     }
 
     // Check for Images
     if (token.value.includes("!") && markdown.charAt(i + 1).includes("[")) {
-      token.value = "![";
-      token.type = "ImageStart";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("ImageStart");
+      tokenTuples.push("![");
+
       i += 1;
       continue;
     }
 
     // Check for Link Brackets
     if (token.value.includes("[")) {
-      token.type = "BracketStart";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("BracketStart");
+      tokenTuples.push("[");
       continue;
     }
 
     if (token.value.includes("]")) {
-      token.type = "BracketEnd";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("BracketEnd");
+      tokenTuples.push("[");
       continue;
     }
 
     // Check for Link definitions
     if (token.value.includes("(")) {
-      token.type = "ParenStart";
+      tokenTuples.push(i.toString());
+      tokenTuples.push("ParenStart");
+      tokenTuples.push("(");
+
       continue;
     }
 
     if (token.value.includes(")")) {
-      token.type = "ParenEnd";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("ParenEnd");
+      tokenTuples.push(")");
+
       continue;
     }
 
     // Check for block quotes
     if (token.value.includes(">") && isWhitespace(markdown.charAt(i + 1))) {
-      token.value = "> ";
-      token.type = "BlockQuote";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("BlockQuote");
+      tokenTuples.push(">");
+
       i += 1;
       continue;
     }
 
     // Check for code blocks
     if (checkForTriplet("`", i, markdown)) {
-      token.value = "```";
-      token.type = "CodeBlock";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("CodeBlock");
+      tokenTuples.push("```");
+
       i += 2;
       continue;
     }
 
     // Check for inline code blocks
     if (token.value.includes("`")) {
-      token.type = "InlineCode";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("InlineCode");
+      tokenTuples.push("`");
+
       continue;
     }
 
     // Check for horizontal lines
     if (checkForTriplet("-", i, markdown)) {
-      token.value = "---";
-      token.type = "HorizontalLine";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("HorizontalLine");
+      tokenTuples.push("---");
+
       i += 2;
       continue;
     }
 
     if (checkForTriplet("=", i, markdown)) {
-      token.value = "===";
-      token.type = "HorizontalLine";
-      tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("HorizontalLine");
+      tokenTuples.push("---");
+
       i += 2;
       continue;
     }
@@ -197,18 +214,20 @@ export function markdownTokenizer(markdown: string): void {
     // We forsure have a character token
     // Check if we should updae the previous token
     if (
-      token.type.includes("Character") &&
       i > 0 &&
       tokens.length > 0 &&
-      tokens[tokens.length - 1].type.includes("Character")
+      tokens[tokens.length - 3].includes("Character")
     ) {
-      tokens[tokens.length - 1].value += token.value;
+      tokens[tokens.length - 1] += tokenValue;
     } else {
       tokens.push(token);
+      tokenTuples.push(i.toString());
+      tokenTuples.push("Character");
+      tokenTuples.push(tokenValue);
     }
   }
 
-  // log(tokens[0].value);
+  log(tokens[6]);
 
-  // return tokens;
+  return tokens;
 }
