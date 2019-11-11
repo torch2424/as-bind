73721fccb3894be1a9dc02e79229c7f3cc03434e
Aaron Turner <aaron@aaronthedev.com>
2019-11-10 23:47:20 -0800

    More parser stuff, working on italics / bold

--- examples/markdown-parser/assembly/parser/parser.ts
+++ examples/markdown-parser/assembly/parser/parser.ts
@@ -10,54 +10,133 @@ export function markdownTokenParser(tokens: Array<Token>): Array<AstNode> {
   ast = new Array<AstNode>(0);
 
   for (let i: i32 = 0; i < tokens.length; i++) {
-    let astNode: AstNode = new AstNode();
-
-    if (token.type === "NewLine") {
-      astNode.type = "NewLine";
-    }
+    let indexOffset = addAstNode(tokens, i);
+    i += indexOffset;
   }
+
+  return ast;
 }
 
 function addAstNode(tokens: Array<Token>, tokenIndex: i32): i32 {
   let astNode: AstNode = new AstNode();
   let token: Token = tokens[tokenIndex];
 
-  if (token.type === "NewLine") {
+  if (token.type == "NewLine") {
     astNode.type = "NewLine";
+    ast.push(astNode);
     return 0;
   }
 
-  if (token.type === "Whitespace") {
+  if (token.type == "Whitespace") {
     astNode.type = "Whitespace";
+    ast.push(astNode);
     return 0;
   }
 
-  if (token.type === "Header") {
-    // Prepare our Index offset
+  if (token.type == "Header") {
+    // Initialize the offset for finding the header
     let indexOffset = 0;
 
-    // TODO: Started falling asleep
-    // Call getHeader AST Node, which can call other AST functions
-    // Add an indexOffstet that way we can adjust accodingly
+    // Find the level of the header
+    let headerLevel = 1;
+    while (
+      tokenIndex + indexOffset + 1 < tokens.length - 1 &&
+      tokens[tokenIndex + indexOffset + 1].type === "Header"
+    ) {
+      headerLevel += 1;
+      indexOffset += 1;
+    }
+
+    // Check if the next value is whitespace
+    if (tokens[tokenIndex + indexOffset + 1].type === "Whitespace") {
+      // We have a header
+
+      // Increase the offset once more
+      indexOffset += 1;
+
+      // Get the content of the header
+      let content: string = getContentOfTokensUntilTokenReached(
+        tokens,
+        tokenIndex + indexOffset + 1,
+        "NewLine"
+      );
+
+      astNode.type = "Header" + headerLevel.toString();
+      astNode.value = content;
+
+      ast.push(astNode);
+      return indexOffset;
+    }
   }
+
+  if (token.type == "Italics") {
+    // TODO: Find the content between the two italics tokens
+    // checkIfTypeIsFoundBeforeOtherType()
+  }
+
+  ast.push(astNode);
+  return 0;
 }
 
-function getHeaderAstNode(tokens: Array<Token>, tokenIndex: i32): AstNode {
-  let astNode: AstNode = new AstNode();
-
-  // Find the level of the header
-  let headerLevel = 1;
-  while (
-    tokenIndex + indexOffset + 1 < tokens.length - 1 &&
-    tokens[tokenIndex + indexOffset + 1].type === "Header"
-  ) {
-    headerLevel += 1;
-    indexOffset += 1;
-  }
-
+function getContentOfTokensUntilTokenReached(
+  tokens: Array<Token>,
+  startTokenIndex: i32,
+  stopTokenType: string
+): string {
   // Get the content of the header
+  let contentTokens: Array<tokens> = getAllTokensUntilTokenReached(
+    tokens,
+    tokenIndex + indexOffset + 1,
+    "NewLine"
+  );
+
+  let content: string = "";
+  for (let i = 0; i < contentTokens.length; i++) {
+    content += token.value;
+  }
+
+  return content;
 }
 
-function getItalicsAstNode(tokens: Array<Token>, tokenIndex: i32): AstNode {}
+function checkIfTypeIsFoundBeforeOtherType(
+  tokens: Array<Token>,
+  startTokenIndex: i32,
+  checkTokenType: string,
+  otherTokenType: string
+): boolean {
+  let checkTokens: Array<tokens> = getAllTokensUntilTokenReached(
+    tokens,
+    tokenIndex + indexOffset + 1,
+    checkTokenType
+  );
+  let otherTokens: Array<tokens> = getAllTokensUntilTokenReached(
+    tokens,
+    tokenIndex + indexOffset + 1,
+    otherTokenType
+  );
 
-function getBoldAstNode(tokens: Array<Token>, tokenIndex: i32): AstNode {}
+  if (checkTokens.length < otherTokens) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getAllTokensUntilTokenReached(
+  tokens: Array<Token>,
+  startTokenIndex: i32,
+  stopTokenType: string
+): Array<Token> {
+  let responseTokens = new Array<Token>();
+
+  for (let i = startTokenIndex; i < tokens.length; i++) {
+    let token = tokens[i];
+    if (token.type == stopTokenType) {
+      i = tokens.length;
+    } else {
+      responseTokens.push(token);
+    }
+  }
+
+  return responseTokens;
+}
