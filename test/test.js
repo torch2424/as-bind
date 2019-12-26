fa423018abcaf437ed860bbac2bda8f811d8ccec
Aaron Turner <aaron@aaronthedev.com>
2019-12-26 00:42:48 -0800

    Added tons of tests, got all type caching done!
    Need to just continue the checklist

--- test/test.js
+++ test/test.js
@@ -216,4 +216,318 @@ describe("asbind", () => {
       });
     });
   });
+
+  describe("type caching", () => {
+    let asbindInstance;
+    let testImportCalledWith = [];
+    let weappedBaseImportObject = {};
+
+    beforeEach(async () => {
+      const importObjectFunction = value => {
+        testImportCalledWith = [value];
+      };
+
+      wrappedBaseImportObject = {
+        ...baseImportObject,
+        test: {
+          testImportString: importObjectFunction,
+          testImportTwoStrings: (value1, value2) => {
+            testImportCalledWith = [value1, value2];
+          },
+          testImportInt8Array: importObjectFunction,
+          testImportUint8Array: importObjectFunction,
+          testImportInt16Array: importObjectFunction,
+          testImportUint16Array: importObjectFunction,
+          testImportInt32Array: importObjectFunction,
+          testImportUint32Array: importObjectFunction,
+          testImportFloat32Array: importObjectFunction,
+          testImportFloat64Array: importObjectFunction
+        }
+      };
+
+      asbindInstance = await asbind.instantiate(
+        wasmBytes,
+        wrappedBaseImportObject
+      );
+    });
+
+    it("should cache bound export function parameter types", () => {
+      assert.equal(asbindInstance.exports.helloWorldTwo.shouldCacheTypes, true);
+
+      const response = asbindInstance.exports.helloWorldTwo("asbind", "world");
+      assert.equal(response, "Hello asbind and world!");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        2
+      );
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes[0].key,
+        "STRING"
+      );
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes[1].key,
+        "STRING"
+      );
+    });
+
+    it("should cache bound export function return types", () => {
+      assert.equal(asbindInstance.exports.helloWorldTwo.shouldCacheTypes, true);
+
+      const response = asbindInstance.exports.helloWorldTwo("asbind", "world");
+      assert.equal(response, "Hello asbind and world!");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedReturnTypes.length,
+        1
+      );
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedReturnTypes[0].key,
+        "STRING"
+      );
+    });
+
+    it("should cache bound import function parameter types", () => {
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.shouldCacheTypes,
+        true
+      );
+      asbindInstance.exports.callTestImportTwoStrings("asbind", "asbind2");
+      assert.equal(testImportCalledWith[0], "asbind");
+      assert.equal(testImportCalledWith[1], "asbind2");
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes.length,
+        2
+      );
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes[0].key,
+        "STRING"
+      );
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes[1].key,
+        "STRING"
+      );
+    });
+
+    // Test cache hits on export params
+    it("should hit cache for export function parameter types", () => {
+      assert.equal(asbindInstance.exports.helloWorldTwo.shouldCacheTypes, true);
+
+      const response = asbindInstance.exports.helloWorldTwo("asbind", "world");
+      assert.equal(response, "Hello asbind and world!");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        2
+      );
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes[0].key,
+        "STRING"
+      );
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes[1].key,
+        "STRING"
+      );
+
+      // Whenever there is a cache miss, as new cached type is assigned, thus, === on objects
+      // Will check the reference, not the contents, so if === is the same, than we had a hit!
+      const oldCachedType =
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes[0];
+      asbindInstance.exports.helloWorldTwo("asbind", "world");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes[0] ===
+          oldCachedType,
+        true
+      );
+    });
+
+    it("should hit the cache for bound export function return types", () => {
+      assert.equal(asbindInstance.exports.helloWorldTwo.shouldCacheTypes, true);
+
+      const response = asbindInstance.exports.helloWorldTwo("asbind", "world");
+      assert.equal(response, "Hello asbind and world!");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedReturnTypes.length,
+        1
+      );
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedReturnTypes[0].key,
+        "STRING"
+      );
+
+      // Whenever there is a cache miss, as new cached type is assigned, thus, === on objects
+      // Will check the reference, not the contents, so if === is the same, than we had a hit!
+      const oldCachedType =
+        asbindInstance.exports.helloWorldTwo.cachedReturnTypes[0];
+      asbindInstance.exports.helloWorldTwo("asbind", "world");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedReturnTypes[0] ===
+          oldCachedType,
+        true
+      );
+    });
+
+    it("should hit the cache for bound import function parameter types", () => {
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.shouldCacheTypes,
+        true
+      );
+      asbindInstance.exports.callTestImportTwoStrings("asbind", "asbind2");
+      assert.equal(testImportCalledWith[0], "asbind");
+      assert.equal(testImportCalledWith[1], "asbind2");
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes.length,
+        2
+      );
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes[0].key,
+        "STRING"
+      );
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes[1].key,
+        "STRING"
+      );
+
+      const oldCachedType =
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes;
+      asbindInstance.exports.callTestImportTwoStrings("asbind", "asbind2");
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes ===
+          oldCachedType,
+        true
+      );
+    });
+
+    it("should support disabling the export type cache", () => {
+      assert.equal(asbindInstance.exports.helloWorldTwo.shouldCacheTypes, true);
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        0
+      );
+
+      asbindInstance.disableExportFunctionTypeCaching();
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.shouldCacheTypes,
+        false
+      );
+
+      const response = asbindInstance.exports.helloWorldTwo("asbind", "world");
+      assert.equal(response, "Hello asbind and world!");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        0
+      );
+    });
+
+    it("should support re-enabling the export type cache", () => {
+      assert.equal(asbindInstance.exports.helloWorldTwo.shouldCacheTypes, true);
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        0
+      );
+
+      asbindInstance.disableExportFunctionTypeCaching();
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.shouldCacheTypes,
+        false
+      );
+
+      const response = asbindInstance.exports.helloWorldTwo("asbind", "world");
+      assert.equal(response, "Hello asbind and world!");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        0
+      );
+
+      asbindInstance.enableExportFunctionTypeCaching();
+
+      assert.equal(asbindInstance.exports.helloWorldTwo.shouldCacheTypes, true);
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        0
+      );
+
+      asbindInstance.exports.helloWorldTwo("asbind", "world");
+      assert.equal(response, "Hello asbind and world!");
+
+      assert.equal(
+        asbindInstance.exports.helloWorldTwo.cachedArgTypes.length,
+        2
+      );
+    });
+
+    it("should support disabling the import type cache", () => {
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.shouldCacheTypes,
+        true
+      );
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes.length,
+        0
+      );
+
+      asbindInstance.disableImportFunctionTypeCaching();
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.shouldCacheTypes,
+        false
+      );
+
+      asbindInstance.exports.callTestImportTwoStrings("asbind", "asbind2");
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes.length,
+        0
+      );
+    });
+
+    it("should support re-enabling the import type cache", () => {
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.shouldCacheTypes,
+        true
+      );
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes.length,
+        0
+      );
+
+      asbindInstance.disableImportFunctionTypeCaching();
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.shouldCacheTypes,
+        false
+      );
+
+      asbindInstance.exports.callTestImportTwoStrings("asbind", "asbind2");
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes.length,
+        0
+      );
+
+      asbindInstance.enableImportFunctionTypeCaching();
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.shouldCacheTypes,
+        true
+      );
+
+      asbindInstance.exports.callTestImportTwoStrings("asbind", "asbind2");
+
+      assert.equal(
+        wrappedBaseImportObject.test.testImportTwoStrings.cachedArgTypes.length,
+        2
+      );
+    });
+  });
 });
