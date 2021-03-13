06e063b1ec3ebc5a7ca99aa76d999f9593a4017c
Surma <surma@surma.dev>
2021-03-13 16:49:02 +0000

    Working towards browser tests

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,20 +1,34 @@
 const { promisify } = require("util");
 const fs = require("fs/promises");
 
+const Express = require("express");
 const Mocha = require("mocha");
 const assert = require("assert");
 const glob = promisify(require("glob"));
+const pptr = require("puppeteer");
 
 const asc = require("assemblyscript/cli/asc");
 
 globalThis.AsBind = require("../dist/as-bind.cjs.js");
+
+// Just used for syntax highlighting
+const html = String.raw;
 
 async function main() {
   await asc.ready;
 
   await compileAllAsc();
 
-  await runTestsInNode();
+  const [nodeFailures, browserFailures] = await Promise.all([
+    runTestsInNode(),
+    runTestsInPuppeteer()
+  ]);
+  if (nodeFailures !== 0 || browserFailures !== 0) {
+    console.log({ nodeFailures, browserFailures });
+    process.exit(1);
+  }
+  console.log("Passed node and browser tests");
+  return;
 }
 main();
 
@@ -54,8 +68,8 @@ async function runTestsInNode() {
     }
   });
 
-  const failures = await runMochaAsync(mocha);
-  console.log({ failures });
+  const numFailures = await runMochaAsync(mocha);
+  return numFailures;
 }
 
 async function runMochaAsync(mocha) {
@@ -63,4 +77,38 @@ async function runMochaAsync(mocha) {
   return new Promise(resolve => {
     const runner = mocha.run(resolve);
   });
+}
+
+const PORT = 50123;
+async function runTestsInPuppeteer() {
+  const testFiles = await glob("./tests/**/test.js");
+  const browser = await pptr.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  page.on("console", async msg =>
+    console[msg._type](
+      "Browser log:",
+      ...(await Promise.all(msg.args().map(v => v.jsonValue())))
+    )
+  );
+
+  const app = Express();
+  app.use("/", Express.static("../"));
+  const server = app.listen(PORT);
+  await page.goto(`http://localhost:${PORT}/test/test-runner.html`);
+  await page.setContent(html`
+    <!DOCTYPE html>
+    <script src="/dist/as-bind.iife.js"></script>
+    <script>
+      self.AsBind = AsBindIIFE;
+    </script>
+  `);
+
+  await page.close();
+  await browser.close();
+  // Just to make sure all the logs come out. There’s probably a better solution.
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  server.close();
+  return 0;
 }
