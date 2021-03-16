72ecf7306c23dc2fc8960ccb8d152be12f6c08a7
Surma <surma@surma.dev>
2021-03-16 19:49:18 +0000

    Don't crash on unexpected imports

---
+++ test/tests/unexpected-import/test.js
@@ -0,0 +1,10 @@
+describe("as-bind", function() {
+  it("should handle unexpected imports gracefully", async function() {
+    const instance = await AsBind.instantiate(this.rawModule, {
+      env: {
+        someFunc() {}
+      }
+    });
+    assert(instance.exports.thing() === -1);
+  });
+});
