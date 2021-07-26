259c341a0458177590a9691d9b54e0d3542faf79
Surma <surma@surma.dev>
2021-07-26 19:54:11 +0100

    Pin non-primitive function parameters (#87)
    * Trying to build a repro case
    
    * Use Rehkitz’ repro
    
    * Fix it
    
    * Getting travis to run after .org to .com migration
    
    * Travis Please!
    
    * Updated the webhook, maybe now travis will update?
    
    * Next day, maybe Travis will be happy now?
    
    * Travis should work for sure now :)
    
    Co-authored-by: Aaron Turner <aaron@aaronthedev.com>

---
+++ test/tests/pinning/test.js
@@ -0,0 +1,27 @@
+describe("as-bind", function() {
+  // Shoutout to @RehkitzDev for the repro
+  it("should not GC strings", function(done) {
+    let num_logs = 0;
+    function string_log(s) {
+      num_logs += 1;
+      assert(!/[^ABCD]/.test(s));
+      if (num_logs === 4) {
+        done();
+      }
+    }
+
+    (async () => {
+      const instance = await AsBind.instantiate(this.rawModule, {
+        asc: { string_log }
+      });
+
+      instance.exports.trash(10000);
+      let a = "A".repeat(30);
+      let b = "B".repeat(30);
+      let c = "C".repeat(60);
+      let d = "D".repeat(60);
+
+      instance.exports.string_parameter(a, b, c, d);
+    })();
+  });
+});
