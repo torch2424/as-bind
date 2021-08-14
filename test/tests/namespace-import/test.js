1f67e59ce3005f6d9892f29e763a85dcf15d07ea
Felipe Gasper <felipe@felipegasper.com>
2021-08-13 21:33:55 -0400

    Teach as-bind to recognize external decorator.

---
+++ test/tests/namespace-import/test.js
@@ -0,0 +1,24 @@
+describe("as-bind", function() {
+  it("should support @external imports", async function() {
+    let from_wasm;
+    let from_wasm2;
+
+    const instance = await AsBind.instantiate(this.rawModule, {
+      my_module: {
+        my_log(str) {
+          from_wasm = str;
+        }
+      },
+      asc: {
+        my_log2(str) {
+          from_wasm2 = str;
+        }
+      }
+    });
+
+    instance.exports.fn();
+
+    assert(from_wasm === "ok");
+    assert(from_wasm2 === "fine");
+  });
+});
